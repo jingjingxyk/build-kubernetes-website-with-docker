2dc5f0dfa16d1f5b4d4aaec7793e3416a4654a44
好吧，你想说啥 <zonghengbaihe521@qq.com>
2020-02-09 19:30:52 +0800

    Create build-docker.sh

---
+++ build-docker.sh
@@ -0,0 +1,7 @@
+#!/bin/env sh 
+HUGO_VERSION='0.64.0'
+TIME=`date "+%Y%m%d"`
+VERSION=${TIME}
+IMAGE="jingjingxyk/kubernetes-website:latest-${VERSION}"
+#docker build -t ${IMAGE} -f ./Dockerfile  .  --force-rm=true --no-cache=true --pull=true  --build-arg HUGO_VERSION=${HUGO_VERSION}
+docker build -t ${IMAGE} -f ./Dockerfile  .    --build-arg HUGO_VERSION=${HUGO_VERSION}

8a2415387ccbb58408351a447364021cd775cb37
好吧，你想说啥 <zonghengbaihe521@qq.com>
2020-02-09 20:55:19 +0800

    Update build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -5,5 +5,5 @@ VERSION=${TIME}
 IMAGE="${DOCKER_IMAGE}${VERSION}"
 #docker build -t ${IMAGE} -f ./Dockerfile  .  --force-rm=true --no-cache=true --pull=true  --build-arg HUGO_VERSION=${HUGO_VERSION}
 #docker build -t ${IMAGE} -f ./Dockerfile  .    --build-arg HUGO_VERSION=${HUGO_VERSION}
-echo "${  DOCKER_PASSWORD }" | docker login  -u ${DOCKER_USER} --password-stdin
+echo "${DOCKER_PASSWORD}" | docker login  -u ${DOCKER_USER} --password-stdin
 #docker push ${IMAGE}

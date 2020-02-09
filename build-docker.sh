256361c4bf9dacc67b4bd39e64a9c29876a41c71
好吧，你想说啥 <zonghengbaihe521@qq.com>
2020-02-09 20:57:18 +0800

    Update build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -4,6 +4,6 @@ TIME=`date "+%Y%m%d"`
 VERSION=${TIME}
 IMAGE="${DOCKER_IMAGE}${VERSION}"
 #docker build -t ${IMAGE} -f ./Dockerfile  .  --force-rm=true --no-cache=true --pull=true  --build-arg HUGO_VERSION=${HUGO_VERSION}
-#docker build -t ${IMAGE} -f ./Dockerfile  .    --build-arg HUGO_VERSION=${HUGO_VERSION}
+docker build -t ${IMAGE} -f ./Dockerfile  .    --build-arg HUGO_VERSION=${HUGO_VERSION}
 echo "${DOCKER_PASSWORD}" | docker login  -u ${DOCKER_USER} --password-stdin
-#docker push ${IMAGE}
+docker push ${IMAGE}

38692459fe596fe30459766651de2dcbaca3b350
好吧，你想说啥 <zonghengbaihe521@qq.com>
2020-02-09 20:46:07 +0800

    Update build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -2,7 +2,7 @@
 HUGO_VERSION='0.64.0'
 TIME=`date "+%Y%m%d"`
 VERSION=${TIME}
-IMAGE="jingjingxyk/kubernetes-website:latest-${VERSION}"
+IMAGE="${DOCKER_IMAGE}${VERSION}"
 #docker build -t ${IMAGE} -f ./Dockerfile  .  --force-rm=true --no-cache=true --pull=true  --build-arg HUGO_VERSION=${HUGO_VERSION}
 #docker build -t ${IMAGE} -f ./Dockerfile  .    --build-arg HUGO_VERSION=${HUGO_VERSION}
 echo "${  DOCKER_PASSWORD }" | docker login  -u ${DOCKER_USER} --password-stdin

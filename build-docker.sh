530d48e2a4fa2470993605e866129990bdf5e221
好吧，你想说啥 <zonghengbaihe521@qq.com>
2020-09-13 11:49:31 +0800

    Update build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -5,6 +5,43 @@ TIME=`date "+%Y%m%d"`
 # VERSION=${TIME}
 # IMAGE="${DOCKER_IMAGE}:${VERSION}"
 
+sudo apt-get update && sudo apt-get install -y apt-transport-https curl
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
+deb https://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm kubectl
+sudo apt-mark hold kubelet kubeadm kubectl
+
+KUBE_VERSION=`kubelet --version |  awk -F ' ' '{print $2}'`
+list=$(kubeadm config images list --kubernetes-version=${KUBE_VERSION})
+for line in ${list}
+do
+    echo ${line}
+    docker pull ${line}
+done
+
+rm -rf ${KUBE_VERSION}
+if [ ! -d ${KUBE_VERSION}  ];then
+  echo "创建文件夹${KUBE_VERSION}";
+  mkdir ./${KUBE_VERSION}
+else
+  echo "${KUBE_VERSION} 文件夹已存在"
+fi
+ls -lah ${KUBE_VERSION}
+for line in ${list}
+do
+   echo ${line}
+   name=$(echo ${line} | sed -r 's/.*\/(.*):.*/\1/')
+   echo ${name}
+   docker save   save -o "./${KUBE_VERSION}/kubernetes-${name}.tar" ${line}
+done
+
+tar -czvf "kubernetes-${KUBE_VERSION}.tar.gz" "./${KUBE_VERSION}"
+
+
+
 git clone https://github.com/kubernetes/website.git
 cd website
 #VERSION=$(git describe --tags `git rev-list --tags --max-count=1`)
@@ -18,6 +55,10 @@ VERSION=${TIME}_${GIT_REVISION}
 IMAGE="${DOCKER_IMAGE}:${VERSION}"
 echo ${IMAGE}
 echo ${VERSION}
+
+
+
+
 
 git checkout ${VERSION}
 cd ..

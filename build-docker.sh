b8d973c1fb9dc6642dd01a1d15ea50016b223555
jingjingxyk <zonghengbaihe521@qq.com>
2021-03-06 16:12:37 +0800

    auto commit  202103061612

--- build-docker.sh
+++ build-docker.sh
@@ -1,18 +1,20 @@
 #!/bin/env sh
 set -eux
 export DOCKER_BUILDKIT=1
-
+sudo ps -ef
+sudo free -m
 HUGO_VERSION='0.64.0'
 TIME=`date "+%Y%m%d"`
 # VERSION=${TIME}
 # IMAGE="${DOCKER_IMAGE}:${VERSION}"
 sudo apt-get update -y
 #sudo apt-get update
-sudo apt-get autoclean            #    清理旧版本的软件缓存
-sudo apt-get clean                 #   清理所有软件缓存
-sudo apt-get autoremove            # 删除系统不再使用的孤立软件
+#sudo apt-get autoclean            #    清理旧版本的软件缓存
+#sudo apt-get clean                 #   清理所有软件缓存
+#sudo apt-get autoremove            # 删除系统不再使用的孤立软件
 
 sudo apt-get remove -y docker docker-engine docker.io containerd runc containernetworking-plugins
+
 sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
@@ -29,42 +31,7 @@ sudo add-apt-repository \
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 
-sudo apt install -y git curl wget sudo python3 python3-pip
-sudo git clone  https://github.com/ceph/ceph.git
-cd  ceph
-sudo  sh -c '/bin/echo -e "\ny\ny"' | sudo apt-get install `cat doc_deps.deb.txt`
-sudo  admin/build-doc
-
-mv   admin/build-doc/output ../ceph-build-docs/ceph-output
-sudo curl --silent --remote-name --location https://github.com/ceph/ceph/raw/octopus/src/cephadm/cephadm
-sudo  mv cephadm ../ceph-build-docs/ceph-output/
-cd ..
-sh ceph-build-docs/docker-build.sh
-
-git clone https://github.com/istio/istio.io.git
-#git clone https://github.com/projectcalico/calico.git
-
-#istio-io website
-cd istio.io
-#ls -lah
-git pull
-make build
-cd ..
-
-
-#docker build -t docker.io/wenba100xie/istio-io-websiete-mirror -f Dockerfile-Istio-io .
-#docker push wenba100xie/istio-io-websiete-mirror:latest
-#cd ..
-##calico-docs
-
-#docker build -t docker.io/wenba100xie/projectcalico-docs:latest -f Dockerfile-calico-io .
-#docker push wenba100xie/projectcalico-docs:latest:latest
-
-
-
-sudo ps -ef
-sudo free -m
- sudo apt-get install -y apt-transport-https curl
+sudo apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
@@ -75,13 +42,8 @@ EOF
 #sudo mkdir info
 #sudo apt-get upgrade -y
 #
-sudo apt-get update -y
+
 sudo apt-get install -y kubelet kubeadm kubectl
-
-
-
-
-
 
 KUBE_VERSION=`kubelet --version |  awk -F ' ' '{print $2}'`
 list=$(kubeadm config images list --kubernetes-version=${KUBE_VERSION})
@@ -110,23 +72,34 @@ done
 tar -czvf "kubernetes-${KUBE_VERSION}.tar.gz" "./${KUBE_VERSION}"
 mkdir tools
 mv "kubernetes-${KUBE_VERSION}.tar.gz" tools
+
+
 cd tools
+
 
 #curl -L https://istio.io/downloadIstio | sh -
 wget https://github.com/istio/istio/releases/download/1.9.0/istio-1.9.0-linux-amd64.tar.gz
 wget https://github.com/istio/istio/releases/download/1.9.0/istioctl-1.9.0-linux-amd64.tar.gz
 wget https://github.com/docker/compose/releases/download/1.27.4/docker-compose-Linux-x86_64
 wget https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.20.0/crictl-v1.20.0-linux-amd64.tar.gz
+
+#dashboard
 # https://github.com/kubernetes/dashboard
 wget -O Kubernetes-Dashboard-v2.0.4.yaml https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.4/aio/deploy/recommended.yaml
+
+
 # calico
-wget -O calico-tigera-operator.yaml  https://docs.projectcalico.org/manifests/tigera-operator.yaml
-wget -O calico-custom-resources.yaml  https://docs.projectcalico.org/manifests/custom-resources.yaml
+# https://calico.dengxiaci.com/getting-started/kubernetes/self-managed-onprem/onpremises#install-calico-with-kubernetes-api-datastore-more-than-50-nodes
+#wget -O calico-tigera-operator.yaml  https://docs.projectcalico.org/manifests/tigera-operator.yaml
+#wget -O calico-custom-resources.yaml  https://docs.projectcalico.org/manifests/custom-resources.yaml
+curl -O -L  https://projectcalico.org/builds/calicoctl
+curl https://docs.projectcalico.org/manifests/calico.yaml -O
+
 # metrics-server
 wget -O metrics-server-components-v0.4.2.yaml https://github.com/kubernetes-sigs/metrics-server/releases/download/v0.4.2/components.yaml
 
 curl --silent --remote-name --location https://github.com/ceph/ceph/raw/octopus/src/cephadm/cephadm
-curl -O -L  https://github.com/projectcalico/calicoctl/releases/download/v3.17.0/calicoctl-linux-amd64
+#curl -O -L  https://github.com/projectcalico/calicoctl/releases/download/v3.17.0/calicoctl-linux-amd64
 wget https://github.com/goharbor/harbor/releases/download/v2.1.1/harbor-online-installer-v2.1.1.tgz
 wget https://github.com/symfony/cli/releases/download/v4.21.2/symfony_linux_amd64.gz
 
@@ -190,6 +163,11 @@ docker push 'wenba100xie/kubernetes-website:latest'
 docker build -t ${IMAGE} -f ./Dockerfile2  .
 if [ "$old_build_tag" = "${IMAGE_TAG}" ];then
    echo "Yes,最新版本kubernetes 安装包已经存在,终止构建推送"
+   echo 'start ceph wiki build'
+   sh build-soft-wiki.sh
+
    exit 0
+else
+  docker push ${IMAGE}
 fi
-docker push ${IMAGE}
+
---
+++ build-soft-wiki.sh
@@ -0,0 +1,34 @@
+#!/bin/env bash
+set -eux
+
+sudo apt install -y git curl wget sudo python3 python3-pip
+sudo git clone  https://github.com/ceph/ceph.git
+cd  ceph
+sudo  sh -c '/bin/echo -e "\ny\ny"' | sudo apt-get install `cat doc_deps.deb.txt`
+sudo  admin/build-doc
+
+mv   admin/build-doc/output ../ceph-build-docs/ceph-output
+sudo curl --silent --remote-name --location https://github.com/ceph/ceph/raw/octopus/src/cephadm/cephadm
+sudo  mv cephadm ../ceph-build-docs/ceph-output/
+cd ..
+sh ceph-build-docs/docker-build.sh
+
+git clone https://github.com/istio/istio.io.git
+#git clone https://github.com/projectcalico/calico.git
+
+#istio-io website
+cd istio.io
+#ls -lah
+git pull
+make build
+cd ..
+
+
+#docker build -t docker.io/wenba100xie/istio-io-websiete-mirror -f Dockerfile-Istio-io .
+#docker push wenba100xie/istio-io-websiete-mirror:latest
+#cd ..
+##calico-docs
+
+#docker build -t docker.io/wenba100xie/projectcalico-docs:latest -f Dockerfile-calico-io .
+#docker push wenba100xie/projectcalico-docs:latest:latest
+

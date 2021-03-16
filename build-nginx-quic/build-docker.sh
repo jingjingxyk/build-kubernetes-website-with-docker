bc678b4986164550945b2c9d6b4e6de133e7412e
jingjingxyk <zonghengbaihe521@qq.com>
2021-03-16 23:56:36 +0800

    git reset

--- build-nginx-quic/build-docker.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-export DOCKER_BUILDKIT=1
-docker build -t wenba100xie/coturn:2020-11-26 -f Dockerfile .
-docker save -o /data/yaokun/coturn-docker-latest.tar wenba100xie/coturn:2020-11-26

46790c7355d6faae7d3c39893d4cc132b503dac0
jingjingxyk <zonghengbaihe521@qq.com>
2021-03-16 12:12:01 +0800

    auto commit  202103161212

--- build-developer-chrome-com.sh
+++ build-developer-chrome-com.sh
@@ -3,6 +3,7 @@ set -eux
 mkdir /tmp/developer-chrome-com
 docker run --rm -v /tmp/developer-chrome-com:/workspace  -w /workspace node:15-alpine \
 /bin/sh -c "
+set -eux
 pwd
 apk add curl git wget
 git clone https://github.com/GoogleChrome/developer.chrome.com.git
@@ -10,7 +11,7 @@ cd developer.chrome.com
 if [ -d node_modules ] ;then rm -rf node_modules; fi
 npm install  -ddd
 npm run production
-cp dist/en/* dist/
+cp -r dist/en/* dist/
 "
 
 cp -r /tmp/developer-chrome-com/developer.chrome.com/dist/  build-developer-chrome-com

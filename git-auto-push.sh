790d959659aaafb0213c3d66904ffe0dffe8b4f9
jingjingxyk <zonghengbaihe521@qq.com>
2021-08-01 16:52:24 +0800

    [2021/08/01 16:52] Auto update by script

--- git-auto-push.sh
+++ git-auto-push.sh
@@ -1,31 +1,25 @@
 #!/bin/env sh
+set -exu
 
-#!/bin/sh
+__CURRENT__=`pwd`
+__DIR__=$(cd "$(dirname "$0")";pwd)
+cd ${__DIR__} &&
+
+
 TIME=`date "+%Y%m%d%H%M"`
+
 git add .
-git commit -a -m "auto commit  ${TIME}"
-git push -u origin master
-
-
+#git commit -a -m "auto commit  ${TIME}"
+git commit -a -m "[`date '+%Y/%m/%d %H:%M'`] Auto update by script"
+git push -u origin dev --force
 
 GIT_REVISION=`git log -1 --pretty=format:"%h"`
 version=${TIME}_${GIT_REVISION}
 tag_name=release-v${version}
-exit 0
+
 #hostname
 #env
 
 ls -al | grep "^-" | awk '{print $9}'
 
 
-
-#!/bin/sh
-__CURRENT__=`pwd`
-__DIR__=$(cd "$(dirname "$0")";pwd)
-cd ${__DIR__} &&
-git add -A && \
-git commit -m "[`date '+%Y/%m/%d %H:%M'`] Auto update by script" && \
-git push -u origin dev --force
-#git push -u origin dev  --force && \
-
-cd ${__CURRENT__}

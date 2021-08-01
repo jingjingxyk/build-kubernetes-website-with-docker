417158944be6ebd1063f278c38a92812cfb71f88
jingjingxyk <zonghengbaihe521@qq.com>
2021-08-01 14:09:22 +0800

    auto commit  202108011409

--- git-auto-push.sh
+++ git-auto-push.sh
@@ -25,7 +25,9 @@ __DIR__=$(cd "$(dirname "$0")";pwd)
 cd ${__DIR__} &&
 git add -A && \
 git commit -m "[`date '+%Y/%m/%d %H:%M'`] Auto update by script" && \
-
-git push -u origin dev  --force && \
-
+git push -u origin dev
+#git push -u origin dev  --force && \
+git checkout master
+git merge dev
+git push -u origin master
 cd ${__CURRENT__}

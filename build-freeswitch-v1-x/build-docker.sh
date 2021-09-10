457e5fa08d728776b7161d60fbe6310cdba44023
jingjingxyk <zonghengbaihe521@qq.com>
2021-09-10 23:58:34 +0800

    change

--- build-freeswitch-v1-x/build-docker.sh
+++ build-freeswitch-v1-x/build-docker.sh
@@ -5,8 +5,9 @@ TIME=`date "+%Y%m%d%H%M"`
 TIME=`date "+%Y%m%d"`
 VERSION="debian-10-buster-v1.10.x-"${TIME}
 IMAGE="wenba100xie/freeswitch:${VERSION}"
+
 export DOCKER_BUILDKIT=1
-docker build -t ${IMAGE} -f Dockerfile .
+docker build -t ${IMAGE} -f Dockerfile . --build-arg proxy_url=$proxy_url --progress=plain
 docker push ${IMAGE}
 aliyun_image="registry.cn-beijing.aliyuncs.com/jingjingxyk-public/app:freeswitch-debian-10-buster-v1.10.x-${VERSION}";
 docker tag ${IMAGE} $aliyun_image

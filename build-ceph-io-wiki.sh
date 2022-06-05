6071e4e58a8bda04d15145494cbe8eb35b1e279e
jingjingxyk <zonghengbaihe521@qq.com>
2022-06-05 17:46:55 +0800

    [2022-06-05T09:46:55Z] Auto update by script

--- build-ceph-io-wiki.sh
+++ build-ceph-io-wiki.sh
@@ -4,7 +4,13 @@ set -eux
 Day=`date "+%Y%m%d"`
 
 sudo apt install -y git curl wget sudo python3 python3-pip
-sudo git clone  https://github.com/ceph/ceph.git
+
+sudo wget -O main.zip  https://github.com/ceph/ceph/archive/refs/heads/main.zip
+sudo unzip main.zip
+sudo mv ceph-main ceph
+
+# sudo git clone  https://github.com/ceph/ceph.git --depth=1 --progress --recursive
+
 cd  ceph
 sudo  sh -c '/bin/echo -e "y\ny\n"' | sudo apt-get install `cat doc_deps.deb.txt`
 sudo  admin/build-doc

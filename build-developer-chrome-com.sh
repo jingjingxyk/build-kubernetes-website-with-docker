268f54ab91439451bbd400595758bc00848a2610
jingjingxyk <zonghengbaihe521@qq.com>
2022-12-29 21:56:33 +0800

    [2022-12-29T13:56:33Z] Auto update by script

--- build-developer-chrome-com.sh
+++ build-developer-chrome-com.sh
@@ -9,7 +9,7 @@ apk add curl git wget python python3
 git clone https://github.com/GoogleChrome/developer.chrome.com.git
 cd developer.chrome.com
 if [ -d node_modules ] ;then rm -rf node_modules; fi
-npm install  -d
+npm ci
 npm run production
 cp -r dist/en/* dist/
 curl -o dist/robots.txt  https://www.xieyaokun.com/robots.txt
